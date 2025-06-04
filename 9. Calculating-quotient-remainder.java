e7746deb08bd7e6cbe1113a55f006b2966f327c6
Soumya Tripathy <130821564+soumya007GitHub@users.noreply.github.com>
2025-06-04 18:37:41 +0530

    Create 9. Calculating-quotient-remainder.java

---
+++ 9. Calculating-quotient-remainder.java
@@ -0,0 +1,12 @@
+// Calculating quotient and remainder using two number
+class Main
+{
+	public static void main(String[] args) {
+	   byte num1 = 17;
+	   byte num2 = 5;
+	   byte quotient = (byte)(num1/num2);
+	   byte remainder = (byte)(num1%num2);
+	   System.out.println("The quotient is: "+quotient);
+	   System.out.println("The remainder is: "+remainder);
+	}
+}
