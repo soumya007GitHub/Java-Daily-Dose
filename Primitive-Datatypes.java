1ac208fccc3270ed5e6b15e859f6233157ee60c8
Soumya Tripathy <130821564+soumya007GitHub@users.noreply.github.com>
2025-06-04 17:47:20 +0530

    Create Primitive-Datatypes.java

---
+++ Primitive-Datatypes.java
@@ -0,0 +1,23 @@
+// If file name is abc.java and Class name is xyz.java then no need to mention `public Class` here in code 
+class Main
+{
+	public static void main(String[] args) {
+	   // Declaring and initializing primitive datatypes in java
+		byte num1 = 3;
+		short num2 = 4;
+		int num3 = 8;
+		float num4 = 1.45f;
+		double num5 = 4.56e5;
+		char character = 'a';
+		boolean isActive = false;
+		long num6 = 888993018191L;
+		System.out.println("byte: "+ num1);
+		System.out.println("short: "+num2);
+		System.out.println("int: "+num3);
+		System.out.println("float: "+num4);
+		System.out.println("double: "+num5);
+		System.out.println("long: "+num6);
+		System.out.println("char: "+character);
+		System.out.println("boolean: "+isActive);
+	}
+}
