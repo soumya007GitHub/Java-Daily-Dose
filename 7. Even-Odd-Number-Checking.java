53e7bb7c44e13bac0e31f5f5d9af2c944d995639
Soumya Tripathy <130821564+soumya007GitHub@users.noreply.github.com>
2025-06-04 18:26:08 +0530

    Create 7. Even-Odd-Number-Checking.java

---
+++ 7. Even-Odd-Number-Checking.java
@@ -0,0 +1,13 @@
+// Even Odd number checking
+class Main
+{
+	public static void main(String[] args) {
+	   byte num = 13;
+	   if(num%2 == 0){
+	       System.out.println("It's an even number");
+	   }
+	   else{
+	       System.out.println("It's an odd number");
+	   }
+	}
+}
