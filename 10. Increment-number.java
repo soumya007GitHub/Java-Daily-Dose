b66a6c59e4f9ea1bd3bc7c4c6ef7eba5e4985b8a
Soumya Tripathy <130821564+soumya007GitHub@users.noreply.github.com>
2025-06-04 18:40:35 +0530

    Create 10. Increment-number.java

---
+++ 10. Increment-number.java
@@ -0,0 +1,12 @@
+// incrementing number by using increment operator
+class Main
+{
+	public static void main(String[] args) {
+	   byte num1 = 10;
+	   byte num2 = 20;
+	   ++num1; // pre-increment
+	   num2++; // post-increment
+	   System.out.println(num1);
+	   System.out.println(num2);
+	}
+}
