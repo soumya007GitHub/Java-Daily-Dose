031eb1995bb5b49904ef2b67a1fc670f05e41b78
Soumya Tripathy <130821564+soumya007GitHub@users.noreply.github.com>
2025-06-04 20:38:59 +0530

    Create 16. Typecasting-double-int.java

---
+++ 16. Typecasting-double-int.java
@@ -0,0 +1,9 @@
+// type-casting from double to int
+class Main
+{
+	public static void main(String[] args) {
+	   double num1 = 12347.873999;
+	   int num2 = (int)num1;
+	   System.out.println(num2);
+	}
+}
