318116c0b001c59e8237c77dc26fa8398f98a05b
Soumya Tripathy <130821564+soumya007GitHub@users.noreply.github.com>
2025-06-04 18:33:15 +0530

    Create 8. Finding-Average.java

---
+++ 8. Finding-Average.java
@@ -0,0 +1,11 @@
+// Calculating average
+class Main
+{
+	public static void main(String[] args) {
+	   byte num1 = 8;
+	   byte num2 = 13;
+	   byte num3 = 4;
+	   float average = (float)(num1+num2+num3)/3;
+	   System.out.println("The average is: "+average);
+	}
+}
