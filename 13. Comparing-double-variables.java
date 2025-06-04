816d872f02faf56dd08484e9fb51765e7927b5c9
Soumya Tripathy <130821564+soumya007GitHub@users.noreply.github.com>
2025-06-04 19:50:12 +0530

    Create 13. Comparing-double-variables.java

---
+++ 13. Comparing-double-variables.java
@@ -0,0 +1,11 @@
+// comparing two double variables
+class Main
+{
+	public static void main(String[] args) {
+	   double num1 = 7.901;
+	   double num2 = 7.901;
+	   double num3 = 7.90;
+	   System.out.println(num1 == num2);
+	   System.out.println(num1 == num3);
+	}
+}
