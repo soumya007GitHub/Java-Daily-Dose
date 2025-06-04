25635c05e3fba8d6c1eff24a297e7c7cc9b71f9c
Soumya Tripathy <130821564+soumya007GitHub@users.noreply.github.com>
2025-06-04 18:45:49 +0530

    Create 11. Arithmetic-operators.java

---
+++ 11. Arithmetic-operators.java
@@ -0,0 +1,13 @@
+// implementing all arithmetic operators
+class Main
+{
+	public static void main(String[] args) {
+	   byte num1 = 20;
+	   byte num2 = 10;
+	   System.out.println("Addition: "+(num1+num2));
+	   System.out.println("Substraction: "+(num1-num2));
+	   System.out.println("Multiplication: "+(num1*num2));
+	   System.out.println("Division: "+(num1/num2));
+	   System.out.println("Modulus: "+(num1%num2));
+	}
+}
