631e10c13e2fe4628835620c58df76775a14dd2f
Soumya Tripathy <130821564+soumya007GitHub@users.noreply.github.com>
2025-06-04 20:44:17 +0530

    Create 17. Typecasting.java

---
+++ 17. Typecasting.java
@@ -0,0 +1,12 @@
+// operator precedence in java
+class Main
+{
+	public static void main(String[] args) {
+	   byte a = 10;
+	   byte b = 20;
+	   byte c = 30;
+	   byte d = 40;
+	   byte result = (byte)(a+b*c/d);
+	   System.out.println(result);
+	}
+}
