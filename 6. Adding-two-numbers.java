513ef1ba4bcee482198824ec171acce714bc8e9c
Soumya Tripathy <130821564+soumya007GitHub@users.noreply.github.com>
2025-06-04 18:21:12 +0530

    Create 6. Adding-two-numbers.java

---
+++ 6. Adding-two-numbers.java
@@ -0,0 +1,19 @@
+// Adding two numbers
+class Main
+{
+	public static void main(String[] args) {
+	   // byte dataType
+	   byte num1 = 3;
+	   byte num2 = 4;
+	   // short dataType
+	   short num3 = 212;
+	   short num4 = 300;
+	   // int dataType
+	   int num5 = 49999;
+	   int num6 = 59999;
+	   
+	   System.out.println("Adding byte numbers: "+(num1+num2));
+	   System.out.println("Adding short numbers: "+(num2+num3));
+	   System.out.println("Adding int numbers: "+(num5+num6));
+	}
+}
