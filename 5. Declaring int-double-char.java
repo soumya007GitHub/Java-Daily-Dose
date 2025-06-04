4bec44baa227fd42d5ebc11d702ee980b7b65e3d
Soumya Tripathy <130821564+soumya007GitHub@users.noreply.github.com>
2025-06-04 18:17:11 +0530

    Create 5. Declaring int-double-char.java

---
+++ 5. Declaring int-double-char.java
@@ -0,0 +1,12 @@
+// Declaring int,double,char and printing values
+class Main
+{
+	public static void main(String[] args) {
+	   int num1 = 5;
+	   double num2 = 9.89;
+	   char character = 'x';
+	   System.out.println("int: "+num1);
+	   System.out.println("double: "+num2);
+	   System.out.println("char: "+character);
+	}
+}
